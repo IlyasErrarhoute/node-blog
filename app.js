bc9534878a7b6363e1f336393f9288fdbf1562d6
IlyasErrarhoute <ilyas.errarhoute@stud.hs-bochum.de>
2024-05-10 21:08:33 +0200

    first push

---
+++ app.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.use(express.static('public'));
+
+//Template Engine
+app.use(expressLayouts);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+app.use('/', require('./server/routes/main'));
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
