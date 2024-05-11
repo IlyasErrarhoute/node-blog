62aaaefe71d1cf74f3be158bd4658a0707206744
Zrida51 <147068121+IlyasErrarhoute@users.noreply.github.com>
2024-05-11 19:57:32 +0200

    database connection added

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 
+const connectDB = require('./server/config/db');
+
+
 const app = express();
 const port = process.env.PORT || 3000;
-
+//connect to DB
+connectDB();
 
 app.use(express.static('public'));
 
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,15 +1,64 @@
 const express = require('express');
 const router = express.Router();
 
+const Post = require('../modules/Post');
+
+
+
+
+
+
+//GET HOME
 
 //
-router.get('', (req, res) => {
+router.get('',async (req, res) => {
     const locals = {
         title: 'Home',
         description: 'Welcome to the home page'
-    };
-res.render('index', { locals });
+    }
+    try{
+        const data = await Post.find();
+        res.render('index', { locals, data });
+
+    }
+    catch(err){
+        console.error(err);
+    }
+
+
+
 });
+
+
+// function insertPostData(){
+//     Post.insertMany([
+//         {
+//             title: 'Biudling  a blog 1',
+//             body: 'This is the body of post 1'
+//         },
+//         {
+//             title: 'Post 2',
+//             body: 'This is the body of post 2'
+//         },
+//         {
+//             title: 'Post 3',
+//             body: 'This is the body of post 3'
+//         }
+//     ]
+    
+
+//     )
+// }
+// insertPostData();   
+
+
+
+
+
+
+
+
+
 router.get('/about', (req, res) => {
 res.render('about');
 });
